b95c1f04bc4ced1ad7f1e3efbf7042ad6724fff2
ramyadav2015 <121161930+ramyadav2015@users.noreply.github.com>
2023-10-27 16:28:53 +0530

    Update InsufficientFundException.java

--- InsufficientFundException.java
+++ InsufficientFundException.java
@@ -1,10 +1,1 @@
-package Exception.BankingException;
 
-public class InsufficientFundException extends Exception {
-	
-	public InsufficientFundException() {
-		
-		super("Insufficient Fund Your Bank Account");
-	}
-
-}
