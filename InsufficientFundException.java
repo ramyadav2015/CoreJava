5d9d559e0cd475c7efdd912bccbd3730db0f845c
ramyadav2015 <121161930+ramyadav2015@users.noreply.github.com>
2023-10-27 16:11:03 +0530

    Add files via upload

---
+++ InsufficientFundException.java
@@ -0,0 +1,10 @@
+package Exception.BankingException;
+
+public class InsufficientFundException extends Exception {
+	
+	public InsufficientFundException() {
+		
+		super("Insufficient Fund Your Bank Account");
+	}
+
+}
